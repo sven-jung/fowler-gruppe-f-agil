7caf72f64804829705217d3d213bc00675fb2228
Sven Jung <intern@svenjung.me>
2021-03-06 13:51:16 +0100

    US8.1 + US 8.2: Moved getCharge() and getFrequentRenterPoints()

--- src/main/java/dhbw/fowler1/videostore/Movie.java
+++ src/main/java/dhbw/fowler1/videostore/Movie.java
@@ -1,6 +1,7 @@
 package dhbw.fowler1.videostore;
 
 public class Movie {
+
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
@@ -23,5 +24,37 @@ public class Movie {
 
     public String getTitle() {
         return _title;
+    }
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        // determine amount for each line
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE)
+                && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
     }
 }
