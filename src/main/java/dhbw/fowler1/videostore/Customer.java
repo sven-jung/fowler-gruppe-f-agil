99242894dccde837d5330230d5465a874290dd6d
Sven Jung <intern@svenjung.me>
2021-03-06 12:10:28 +0100

    US3: Move amount calculation to rental

--- src/main/java/dhbw/fowler1/videostore/Customer.java
+++ src/main/java/dhbw/fowler1/videostore/Customer.java
@@ -19,7 +19,7 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             
             Rental each = (Rental) rentals.nextElement();
-            double thisAmount = amountFor(each);
+            double thisAmount = each.getCharge();
          
             // add frequent renter points
             frequentRenterPoints ++;
@@ -47,24 +47,4 @@ public class Customer {
         return _name;
     }
     
-    private double amountFor(Rental aRental) {
-        double result = 0;
-         // determine amount for each line
-            switch (aRental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    result += 2;
-                    if (aRental.getDaysRented() > 2)
-                        result += (aRental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    result += aRental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    result += 1.5;
-                    if (aRental.getDaysRented() > 3)
-                        result += (aRental.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-         return result;
-    } 
 }
