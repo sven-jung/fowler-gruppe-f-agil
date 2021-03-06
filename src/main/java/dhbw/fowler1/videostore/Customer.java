b591b8c1f20a23aedace5e9a0a031c2df473922b
Sven Jung <intern@svenjung.me>
2021-03-06 12:48:43 +0100

    US4: thisAmount Removed

--- src/main/java/dhbw/fowler1/videostore/Customer.java
+++ src/main/java/dhbw/fowler1/videostore/Customer.java
@@ -19,7 +19,6 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             
             Rental each = (Rental) rentals.nextElement();
-            double thisAmount = each.getCharge();
          
             // add frequent renter points
             frequentRenterPoints ++;
@@ -29,8 +28,8 @@ public class Customer {
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
